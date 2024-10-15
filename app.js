75b6c076cf9dcfe73dc81f6374862225cb318a51
Mahadi Hasan Sumon <73355302+mahfolio@users.noreply.github.com>
2024-10-16 02:56:41 +0800

    Write a function to generate a random choice for the computer

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Test file");
+function getComputerChoice() {
+    let computerChoice;
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    switch (randomNumber) {
+        case 1:
+            computerChoice = "Rock";     
+            break;
+
+        case 2:
+            computerChoice = "Paper";
+            break;
+
+        case 3:
+            computerChoice = "Scissors";
+            break;
+    
+        default:
+            break;
+    }
+
+    return computerChoice;
+}
+
