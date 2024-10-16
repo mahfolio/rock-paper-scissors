09dc7e8f2375a965b252a1cc6d38e5233be992dc
Mahadi Hasan Sumon <73355302+mahfolio@users.noreply.github.com>
2024-10-17 01:30:27 +0800

    Write a function for the main game logic

--- app.js
+++ app.js
@@ -1,30 +1,78 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let computerChoice;
     let randomNumber = Math.floor(Math.random() * 3) + 1;
 
     switch (randomNumber) {
         case 1:
-            computerChoice = "Rock";     
+            computerChoice = "rock";     
             break;
 
         case 2:
-            computerChoice = "Paper";
+            computerChoice = "paper";
             break;
 
         case 3:
-            computerChoice = "Scissors";
+            computerChoice = "scissors";
             break;
     
         default:
             break;
     }
-
+    // console.log(computerChoice);
     return computerChoice;
 }
 
 function getHumanChoice() {
     let humanChoice = prompt("Select your weapon");
-    
+    // console.log(humanChoice);
     return humanChoice;
 }
 
+function playRound(manChoice, computerChoice) {
+    let humanChoice = manChoice.toLowerCase();
+    // console.log(humanChoice);
+    let message;
+
+    if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            message = "You win! Paper beats Rock.";
+            humanScore += 10;
+        } else if (computerChoice === "scissors") {
+            message = "You lose! Scissors beat Paper.";
+            computerScore += 10;
+        } else {
+            message = "Draw! Nobody wins. Try again.";
+        }
+    } else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            message = "You lose! Paper beats Rock";
+            computerScore += 10;
+        } else if (computerChoice === "scissors") {
+            message = "You win! Rock beats Scissors";
+            humanScore += 10;
+        } else {
+            message = "Draw! Nobody wins. Try again.";
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            message = "You win! Scissors beat Paper.";
+            humanScore += 10;
+        } else if (computerChoice === "rock") {
+            message = "You lose! Rock beats Scissors.";
+            computerScore += 10;
+        } else {
+            message = "Draw! Nobody winds. Try again.";
+        }
+    }
+
+    // console.log(message);
+    return message;
+}
+
+let playerChoice = getHumanChoice();
+let machineChoice = getComputerChoice();
+
+playRound(playerChoice, machineChoice);
