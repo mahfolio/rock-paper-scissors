5fe35cc9088649d7a661cd243e811eefec82ddb5
Mahadi Hasan Sumon <73355302+mahfolio@users.noreply.github.com>
2025-02-06 00:09:57 +0800

    Update the event handler logic to success

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
+let humanScore = 0;
+let computerScore = 0;
+let finalResult;
+
+function playRound(manChoice, machineChoice) {
+    // console.log(humanChoice);
+    let message;
+    if (manChoice === "paper") {
+        if (machineChoice === "rock") {
+            message = "You win! Paper beats Rock.";
+            humanScore += 1;
+        } else if (machineChoice === "scissors") {
+            message = "You lose! Scissors beat Paper.";
+            computerScore += 1;
+        } else {
+            message = "Draw! Nobody wins. Try again.";
+        }
+    } else if (manChoice === "rock") {
+        if (machineChoice === "paper") {
+            message = "You lose! Paper beats Rock";
+            computerScore += 1;
+        } else if (machineChoice === "scissors") {
+            message = "You win! Rock beats Scissors";
+            humanScore += 1;
+        } else {
+            message = "Draw! Nobody wins. Try again.";
+        }
+    } else if (manChoice === "scissors") {
+        if (machineChoice === "paper") {
+            message = "You win! Scissors beat Paper.";
+            humanScore += 1;
+        } else if (machineChoice === "rock") {
+            message = "You lose! Rock beats Scissors.";
+            computerScore += 1;
+        } else {
+            message = "Draw! Nobody wins. Try again.";
+        }
+    }
+    // console.log(message);
+    return message;
+}
 
 function getComputerChoice() {
     let computerChoice;
@@ -9,82 +50,40 @@ function getComputerChoice() {
     // console.log(computerChoice);
     return computerChoice;
 }
-// Change the following function to getHumanChoice interactively using buttons
-function getHumanChoice() {
-    let humanChoice = "";
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            humanChoice = button.id;
-        });
-    });
-    return humanChoice;
-}
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let finalResult;
-
-    function playRound(manChoice, computerChoice) {
-        let humanChoice = manChoice.toLowerCase();
-        // console.log(humanChoice);
-        let message;
-    
-        if (humanChoice === "paper") {
-            if (computerChoice === "rock") {
-                message = "You win! Paper beats Rock.";
-                humanScore += 1;
-            } else if (computerChoice === "scissors") {
-                message = "You lose! Scissors beat Paper.";
-                computerScore += 1;
-            } else {
-                message = "Draw! Nobody wins. Try again.";
-            }
-        } else if (humanChoice === "rock") {
-            if (computerChoice === "paper") {
-                message = "You lose! Paper beats Rock";
-                computerScore += 1;
-            } else if (computerChoice === "scissors") {
-                message = "You win! Rock beats Scissors";
-                humanScore += 1;
-            } else {
-                message = "Draw! Nobody wins. Try again.";
-            }
-        } else if (humanChoice === "scissors") {
-            if (computerChoice === "paper") {
-                message = "You win! Scissors beat Paper.";
-                humanScore += 1;
-            } else if (computerChoice === "rock") {
-                message = "You lose! Rock beats Scissors.";
-                computerScore += 1;
-            } else {
-                message = "Draw! Nobody winds. Try again.";
-            }
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let humanChoice = button.id;
+        let computerChoice = getComputerChoice();
+        let roundResult = playRound(humanChoice, computerChoice);
+        
+        if (humanScore === 5) {
+            console.log(humanScore);
+            console.log(computerScore);
+            finalResult = "You won. Congratulations!";
+            humanScore = 0;
+            computerScore = 0;
+        } else if (computerScore === 5) {
+            console.log(humanScore);
+            console.log(computerScore);
+            finalResult = "You lost. Better luck next time.";
+            humanScore = 0;
+            computerScore = 0;
+        } else if (humanScore === 5 && computerScore === 5) {
+            console.log(humanScore);
+            console.log(computerScore);
+            finalResult = "It is a draw. Play again."
+            humanScore = 0;
+            computerScore = 0;
         }
-        // console.log(message);
-        return message;
-    }
 
-    let playerChoice = getHumanChoice();
-    let machineChoice = getComputerChoice();
-    console.log(playRound(playerChoice, machineChoice));
-    console.log(`Human Score: ${humanScore}`);
-    console.log(`Computer Score: ${computerScore}`);
+        console.log(roundResult);
+        console.log(humanScore);
+        console.log(computerScore);
+        console.log(finalResult);
+    });
+});
 
-    
-
-    if (humanScore > computerScore) {
-        finalResult = "You won. Congratulations!";
-    } else if (humanScore < computerScore) {
-        finalResult = "You lost. Better luck next time.";
-    } else {
-        finalResult = "It is a draw. Play again."
-    }
-
-    console.log(finalResult);
-}
-
-playGame();
 
 
